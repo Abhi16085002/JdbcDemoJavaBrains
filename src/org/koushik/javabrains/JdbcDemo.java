4093d577b3d387e3552620bd12a2e31878d5c419
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-05-17 22:16:33 +0530

    Spring Tutorial 43 - Named Parameter JDBC Template

--- src/org/koushik/javabrains/JdbcDemo.java
+++ src/org/koushik/javabrains/JdbcDemo.java
@@ -13,9 +13,9 @@ public class JdbcDemo {
 		JdbcDaoImpl dao = context.getBean("jdbcDaoImpl",JdbcDaoImpl.class);
 //		Circle circle = dao.getCircle(2);
 //		System.out.println(circle.getName());
-//		dao.insertCircle(new Circle(4,"fourth Circle"));
-//		System.out.println(dao.getAllCircles().size());
-		dao.createTriangle();
+		dao.insertCircle(new Circle(6,"sixth Circle"));
+		System.out.println(dao.getAllCircles().size());
+//		dao.createTriangle();
 	}
 
 }
--- src/org/koushik/javabrains/dao/JdbcDaoImpl.java
+++ src/org/koushik/javabrains/dao/JdbcDaoImpl.java
@@ -14,6 +14,9 @@ import org.koushik.javabrains.model.Circle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -21,6 +24,8 @@ public class JdbcDaoImpl {
 	
 	private DataSource dataSource;
 	private JdbcTemplate jdbcTemplate ;
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
 	public DataSource getDataSource() {
 		return dataSource;
 	}
@@ -29,6 +34,7 @@ public class JdbcDaoImpl {
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
 	}
 	
 	public int getCircleCount() {
@@ -51,9 +57,16 @@ public class JdbcDaoImpl {
 		return jdbcTemplate.query(sql, new CircleMapper());
 	}
 
+//	public void insertCircle(Circle circle) {
+//		String sql = "Insert into circle (ID,name) values (?,?) ";
+//		jdbcTemplate.update(sql, new Object[] {circle.getId(), circle.getName()} );
+//	}
+	
 	public void insertCircle(Circle circle) {
-		String sql = "Insert into circle (ID,name) values (?,?) ";
-		jdbcTemplate.update(sql, new Object[] {circle.getId(), circle.getName()} );
+		String sql = "Insert into circle (ID,name) values (:id,:name) ";
+		SqlParameterSource namedParameters = new MapSqlParameterSource("id",circle.getId())
+				.addValue("name", circle.getName());
+		namedParameterJdbcTemplate.update(sql, namedParameters);	
 	}
 	
 	public void createTriangle() {
